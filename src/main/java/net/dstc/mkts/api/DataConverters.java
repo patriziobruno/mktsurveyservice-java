ebe81ec2d162f4b59b92df426db9971e44c26f8b
Patrizio Bruno <desertconsulting@gmail.com>
2016-10-14 16:04:03 +0200

    implemented an object mapper

---
+++ src/main/java/net/dstc/mkts/api/DataConverters.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2016 eul0860.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.dstc.mkts.api;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import net.dstc.mkts.data.SurveyDAO;
+import net.dstc.mkts.data.SurveyDO;
+import net.dstc.mkts.data.SurveyStatus;
+import net.dstc.mkts.data.SurveyTargetDO;
+
+/**
+ *
+ * @author eul0860
+ */
+public class DataConverters {
+
+    private static class Helper {
+
+        private final static Map<Class, Converter> INSTANCES = new ConcurrentHashMap<>();
+    }
+
+    public static Converter getConverter(SurveyDAO dao) {
+        Converter converter;
+        Class cl = dao.getClass();
+        if (!Helper.INSTANCES.containsKey(cl)) {
+            converter = new Converter(dao);
+            Helper.INSTANCES.put(cl, converter);
+        } else {
+            converter = Helper.INSTANCES.get(cl);
+        }
+        return converter;
+    }
+
+    public static class Converter {
+
+        private final SurveyDAO data;
+
+        private Converter(SurveyDAO data) {
+            this.data = data;
+        }
+
+        public SurveyDTO surveyDOtoDTO(SurveyDO dataObject) {
+            final SurveyTargetDTO target = surveyTargetDOtoDTO(dataObject.getTarget());
+            return new SurveyDTO() {
+                {
+                    setId(dataObject.getId());
+                    setTitle(dataObject.getTitle());
+                    setStatus(dataObject.getStatus());
+                    setStartDate(dataObject.getStartDate());
+                    setTarget(target);
+                }
+            };
+        }
+
+        public SurveyTargetDTO surveyTargetDOtoDTO(SurveyTargetDO targetSrc) {
+            SurveyTargetDTO target = null;
+            if (targetSrc != null) {
+                target = new SurveyTargetDTO() {
+                    {
+                        setCountry(targetSrc.getCountry());
+                        setAgeRange(new int[]{targetSrc.getMinAge(), targetSrc.getMaxAge()});
+                        setIncomeRange(new int[]{targetSrc.getMinIncome(), targetSrc.getMaxIncome()});
+                    }
+                };
+            }
+            return target;
+        }
+
+        public SurveyDO surveyDTOtoDO(SurveyDTO dto) {
+            SurveyDO survey = data.createSurvey();
+            survey.setId(dto.getId());
+            survey.setStartDate(dto.getStartDate());
+            survey.setStatus(SurveyStatus.NEW);
+            survey.setTitle(dto.getTitle());
+            survey.setTarget(surveyTargetDTOtoDO(dto.getTarget()));
+
+            return survey;
+        }
+
+        public SurveyTargetDO surveyTargetDTOtoDO(SurveyTargetDTO dto) {
+            SurveyTargetDO target = null;
+
+            if (dto != null) {
+                target = data.createSurveyTarget();
+                target.setCountry(dto.getCountry());
+                target.setGender(target.getGender());
+
+                int[] ageRange = dto.getAgeRange();
+                if (ageRange != null && ageRange.length > 1) {
+                    target.setMaxAge(dto.getAgeRange()[1]);
+                    target.setMinAge(dto.getAgeRange()[0]);
+                }
+
+                int[] incomeRange = dto.getIncomeRange();
+                if (incomeRange != null && incomeRange.length > 1) {
+                    target.setMaxIncome(dto.getIncomeRange()[1]);
+                    target.setMinIncome(dto.getIncomeRange()[0]);
+                }
+            }
+            return target;
+        }
+    }
+}
--- src/main/java/net/dstc/mkts/api/MarketingSurveyApiImpl.java
+++ src/main/java/net/dstc/mkts/api/MarketingSurveyApiImpl.java
@@ -21,9 +21,7 @@ import javax.annotation.ManagedBean;
 import javax.annotation.Resource;
 import javax.inject.Inject;
 import net.dstc.mkts.data.SurveyDO;
-import net.dstc.mkts.data.SurveyTargetDO;
 import net.dstc.mkts.data.SurveyDAO;
-import net.dstc.mkts.data.SurveyStatus;
 
 /**
  *
@@ -36,108 +34,36 @@ public class MarketingSurveyApiImpl implements MarketingSurveyApi {
     @Inject
     private SurveyDAO data;
 
+    private DataConverters.Converter converter;
+
+    private DataConverters.Converter getConverter() {
+        if (converter == null) {
+            converter = DataConverters.getConverter(data);
+        }
+        return converter;
+    }
+
     @Override
     public Collection<SurveyDTO> getList(SurveyDTO query) {
         SurveyDO daoQuery = null;
 
         if (query != null) {
-            daoQuery = data.createSurvey();
-
-            daoQuery.setId(query.getId());
-            daoQuery.setStartDate(query.getStartDate());
-            daoQuery.setStatus(query.getStatus());
-            daoQuery.setTitle(query.getTitle());
-
-            SurveyTargetDTO targetq = query.getTarget();
-            if (targetq != null) {
-                SurveyTargetDO target = data.createSurveyTarget();
-                daoQuery.setTarget(target);
-                target.setCountry(targetq.getCountry());
-                target.setGender(targetq.getGender());
-                int[] ageRange = targetq.getAgeRange();
-                if (ageRange != null && ageRange.length > 1) {
-                    target.setMinAge(ageRange[0]);
-                    target.setMaxAge(ageRange[1]);
-                }
-                int[] incomeRange = targetq.getIncomeRange();
-                if (incomeRange != null && incomeRange.length > 1) {
-                    target.setMinIncome(incomeRange[0]);
-                    target.setMaxIncome(incomeRange[1]);
-                }
-            }
+            daoQuery = getConverter().surveyDTOtoDO(query);
         }
 
-        return data.list(daoQuery).stream().map(s -> {
-            final SurveyTargetDTO target = new SurveyTargetDTO();
-            if (s.getTarget() != null) {
-                target.setCountry(s.getTarget().getCountry());
-                target.setAgeRange(new int[]{s.getTarget().getMinAge(), s.
-                    getTarget().getMaxAge()});
-                target.setIncomeRange(new int[]{s.getTarget().getMinIncome(), s.
-                    getTarget().getMaxIncome()});
-            }
-            return new SurveyDTO() {
-                {
-                    setId(s.getId());
-                    setTitle(s.getTitle());
-                    setStatus(s.getStatus());
-                    setStartDate(s.getStartDate());
-                    setTarget(target);
-                }
-            };
-        }).collect(Collectors.toList());
+        return data.list(daoQuery).stream().map(s -> getConverter().surveyDOtoDTO(s))
+                .collect(Collectors.toList());
     }
 
     @Override
     public void insert(SurveyDTO survey) {
-        SurveyDO s = data.createSurvey();
-        s.setStartDate(survey.getStartDate());
-        s.setStatus(SurveyStatus.NEW);
-        s.setTitle(survey.getTitle());
-
-        SurveyTargetDTO target = survey.getTarget();
-        if (target != null) {
-            SurveyTargetDO t = data.createSurveyTarget();
-            t.setCountry(target.getCountry());
-            t.setGender(t.getGender());
-            
-            int[] ageRange = target.getAgeRange();
-            if (ageRange != null && ageRange.length > 1) {
-                t.setMaxAge(target.getAgeRange()[1]);
-                t.setMinAge(target.getAgeRange()[0]);
-            }
-
-            int[] incomeRange = target.getIncomeRange();
-            if (incomeRange != null && incomeRange.length > 1) {
-                t.setMaxIncome(target.getIncomeRange()[1]);
-                t.setMinIncome(target.getIncomeRange()[0]);
-            }
-
-            s.setTarget(t);
-        }
+        SurveyDO s = getConverter().surveyDTOtoDO(survey);
         data.add(s);
     }
 
     @Override
     public void update(SurveyDTO survey) {
-        SurveyDO s = data.createSurvey();
-        s.setId(survey.getId());
-        s.setStartDate(survey.getStartDate());
-        s.setStatus(survey.getStatus());
-        s.setTitle(survey.getTitle());
-
-        SurveyTargetDTO target = survey.getTarget();
-        if (target != null) {
-            SurveyTargetDO t = data.createSurveyTarget();
-            t.setCountry(target.getCountry());
-            t.setGender(t.getGender());
-            t.setMaxAge(target.getAgeRange()[1]);
-            t.setMinAge(target.getAgeRange()[0]);
-            t.setMaxIncome(target.getIncomeRange()[1]);
-            t.setMinIncome(target.getIncomeRange()[0]);
-
-            s.setTarget(t);
-        }
+        SurveyDO s = getConverter().surveyDTOtoDO(survey);
         data.update(s);
     }
 
