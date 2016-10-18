fb3a4314b4f3aa43fe7c3fb46907a071159be71d
Patrizio Bruno <desertconsulting@gmail.com>
2016-10-18 17:12:03 +0200

    removed redundant and unused constructors

--- src/main/java/net/dstc/mkts/rest/auth/NotAuthException.java
+++ src/main/java/net/dstc/mkts/rest/auth/NotAuthException.java
@@ -16,7 +16,6 @@
 package net.dstc.mkts.rest.auth;
 
 import javax.ws.rs.NotAuthorizedException;
-import javax.ws.rs.core.Response;
 
 /**
  *
@@ -24,7 +23,7 @@ import javax.ws.rs.core.Response;
  */
 public class NotAuthException extends NotAuthorizedException {
 
-    private String wwwAuthHeader;
+    private final String wwwAuthHeader;
 
     public String getWwwAuthHeader() {
         return wwwAuthHeader;
@@ -33,42 +32,5 @@ public class NotAuthException extends NotAuthorizedException {
     public NotAuthException(String wwwAuthHeader) {
         super("unauthorized");
         this.wwwAuthHeader = wwwAuthHeader;
-    }
-
-    public NotAuthException(Object challenge, Object... moreChallenges) {
-        super(challenge, moreChallenges);
-    }
-
-    public NotAuthException(Response response) {
-        super(response);
-    }
-
-    public NotAuthException(Response response, Throwable cause) {
-        super(response, cause);
-    }
-
-    public NotAuthException(String message, Response response) {
-        super(message, response);
-
-    }
-
-    public NotAuthException(String message, Response response,
-            Throwable cause) {
-        super(message, response, cause);
-    }
-
-    public NotAuthException(String message, Object challenge,
-            Object... moreChallenges) {
-        super(message, challenge, moreChallenges);
-    }
-
-    public NotAuthException(Throwable cause, Object challenge,
-            Object... moreChallenges) {
-        super(cause, challenge, moreChallenges);
-    }
-
-    public NotAuthException(String message, Throwable cause, Object challenge,
-            Object... moreChallenges) {
-        super(message, cause, challenge, moreChallenges);
     }
 }
